da5941d677bfaf7ba121c3cce370ebc39e8c1824
monton <mini-mini9999@hotmail.com>
2018-01-04 08:51:00 +0700

    4_1_2561 v1.0

---
+++ Coachdev/Scripts/GenJS/_layout.js
@@ -0,0 +1,32 @@
+﻿$('#map1').css("height", window.innerHeight);
+//$('#eight').css("height", window.innerHeight -200);
+//$('#side').css("height", window.innerHeight - 10);
+
+//$('#spliting').css("height", window.innerHeight);
+//var w = window.innerWidth;
+//console.log($('#spliting'));
+
+//var _split = $('#spliting').height();
+//$('#map1').css("height", _split - 300);
+//$('#eight').css("height", _split - 300);
+//$('#map1').css("height", 649);
+//$('#eight').css("height", 649);
+
+//var _eight = $('#eight').height();
+//$('#nine').css("height", _eight - 50);
+//$('#nine').css("height", 100);
+
+//console.log("height", window.innerHeight);
+
+
+
+
+
+$(function () {
+    $('#datetimepicker4').datepicker();
+});
+//Split(['#eight', '#nine'], {
+//    direction: 'vertical'
+//    //sizes: ['200px', '500px']
+
+//});
